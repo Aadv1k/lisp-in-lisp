97237d3779aa24b3673b41395d1958e26d569bf8
Aadv1k <aadv1k@outlook.com>
2023-05-10 19:43:04 +0530

    Implemented the basic arithmetic parser

--- main.lisp
+++ main.lisp
@@ -12,100 +12,124 @@
   )
 )
 
-
-;;(push tokens (lil-split-expr (subseq str (+ i 1) (nth 1 (find-next-char-from-index str #\) 0)))))
-
-(defun lil-parse-expr (str) 
-  (reverse (
-            map 'list (lambda (elem) 
-                        (cond ((numberp 
-                                 (parse-integer elem :junk-allowed t)
-                                 ) (list token-number (parse-integer elem))) (t (list token-symbol elem)))) (utl-split-string str " ")))
+(defun string-get-last-char (str)
+  (list (char str (- (length str) 1)) (- (length str) 1))
 )
 
-(defun lil-split-expr (str)
-  "Will tokenize expression within parenthesis"
-  (let ((tokens (list)) (trunc-str ""))
-    (dotimes (i (length str)) 
+
+(defun find-next-space-or-end (start str)
+  (let ((index (loop for i from start below (length str)
+                    when (or (char= (char str i) #\Space)
+                             (char= (char str i) #\Newline)
+                             (char= (char str i) #\Tab)
+                             (char= (char str i) #\))
+
+                             )
+                    return i
+                    finally (return (length str)))))
+    (list index (- index start))))
+
+
+
+(defun tokenize (str)
+  (let ((expr 
+          (if (char= (char str 0) #\()
+            (subseq str 1 (nth 1 (string-get-last-char str)))
+            nil
+            )) 
+        (tokens '()))
+
+    (dotimes (i (length expr))
       (cond 
-        ((char= (char str i) #\() (progn 
-              (push (list token-lp) tokens)
-              )
-            )
-        ((char= (char str i) #\)) (push (list token-rp) tokens))
-       (t (progn (setq trunc-str (concatenate 'string trunc-str (string (char str i))))))
+        ((char= (char expr i) #\+) 
+         (push (list token-symbol (char expr i)) tokens)
+         )
+        ((char= (char expr i) #\/) 
+         (push (list token-symbol (char expr i)) tokens)
+         )
+
+        ((char= (char expr i) #\%)
+         (push (list token-symbol (char expr i)) tokens)
+         )
+
+        ((char= (char expr i) #\-) 
+         (push (list token-symbol (char expr i)) tokens)
+         )
+
+        ((char= (char expr i) #\() 
+         (push (list token-lp (char expr i)) tokens)
+         )
+
+        (
+         (and (digit-char-p (char expr i)) t)
+         (progn
+           (let ((num (subseq expr i (- (nth 0 (find-next-space-or-end i expr)) 0))))
+             (push (list token-number (parse-integer num)) tokens)
+             (setq i (+ i (nth 1 (find-next-space-or-end i expr))))
+             )
+           )
+         )
+        (t ()))
       )
-   )
+    (reverse tokens)))
 
 
-  (lil-parse-expr trunc-str))
-)
+
+;;((SYMBOL #\+) (NUMBER 34) (LPAREN #\() (SYMBOL #\-) (NUMBER 36) (NUMBER 1))
 
 
-(defun do-op-on-stack (op stack) 
-  (let ((result 0)) 
+
+(defun lil-eval-stack (stack)
+  (let ((result 0) (local-stack '()))
     (dotimes (i (length stack))
-      (cond
-        ((string= op "+") (setq result (+ result (nth i stack)))) 
-        ((string= op "-") (setq result (- result (nth i stack)))) 
-        ((string= op "/") (setq result (/ result (nth i stack)))) 
-        ((string= op "*") (setq result (* (if (eql result 0) 1 result) (nth i stack)))) 
-      )
-    )
-  result)
-)
-
-(defun lil-sus (expr)
-  (let ((stack '()))
-  (dotimes (i (length expr))
-    (let ((token (nth i expr)))
       (cond 
-        ((eql token-number (nth 0 token)) (push (nth 1 token) stack))
-        ((eql token-symbol (nth 0 token)) (progn
-            (let ((computed (do-op-on-stack (nth 1 token) stack)))
-              (setq stack ())
-              (push computed stack)
-            )
+        ((numberp (nth i stack)) (push (nth i stack) local-stack))
+        ((char= (nth i stack) #\+) 
+         (let ((computed (reduce #'+ (mapcar #'identity local-stack))))
+          (setq local-stack '())
+          (push computed local-stack)
         ))
+
+        ((char= (nth i stack) #\-) 
+         (let ((computed (reduce #'- (mapcar #'identity local-stack))))
+          (setq local-stack '())
+          (push computed local-stack)
+        ))
+
+        ((char= (nth i stack) #\/) 
+         (let ((computed (reduce #'/ (mapcar #'identity local-stack))))
+          (setq local-stack '())
+          (push computed local-stack)
+        ))
+
+
+        ((char= (nth i stack) #\*) 
+         (let ((computed (reduce #'* (mapcar #'identity local-stack))))
+          (setq local-stack '())
+          (push computed local-stack)
+        ))
+      ))
+    local-stack))
+
+(defun lil-parse (tokens) "Expects a list of tokens"
+  (let ((stack '()) (token nil) (token-type nil) (token-value nil) (paren-open t))
+    (dotimes (i (length tokens)) 
+      (setq token (nth i tokens))
+      (setq token-type (nth 0 token))
+      (setq token-value (nth 1 token))
+
+      (cond 
+        ((and (eql token-type #\() (not paren-open)) 
+         (setq paren-open t))
+        ((and (eql token-type #\)) paren-open) 
+         (setq paren-open nil))
+        ((eql token-type 'SYMBOL) 
+         (push token-value stack))
+        ((eql token-type 'NUMBER) 
+         (push token-value stack))
       )
     )
-  )
-  stack)
+  (lil-eval-stack stack))
 )
 
-;;(print (lil-split-expr  "(+ 1 (+ 1 (* 1 2)))"))
-
-(print (lil-sus (lil-split-expr "(+ 1 (- 0 (* 1 2)))")))
-(terpri)
-
-
-(defun lil-tokenize-line (str)
-  (let ((tokens (list)) (cursor 0) (paren-open t))
-    (dotimes (i (length str))
-      (cond
-        ((char= (char str i) #\() (push `(,token-lp ,i) tokens)) 
-        ((char= (char str i) #\)) (push `(,token-rp ,i) tokens)) 
-        ((char= (char str i) #\+) (push `(,token-symbol ,i ,(char str i)) tokens)) 
-        ((char= (char str i) #\-) (push `(,token-symbol ,i ,(char str i)) tokens)) 
-        ((digit-char-p (char str i)) (progn 
-              (print (char str i))
-              (print (find-next-char-from-index str #\Space i))
-              ;;(print (subseq str 0 (nth 0 (find-next-char-from-index str #\Space i))))
-             ;;(incf i (+ 0 (nth 1 (find-next-char-from-index str #\Space i))))
-         ))
-      )
-    )
-    
-  (reverse tokens))
-)
-
-
-(defun lil-tokenize (str)
-  (let ((lines (utl-split-by-lines str)))
-    (let ((program '()))
-      (dotimes (i (length lines))
-	(push (list (lil-tokenize-line (nth i lines)) i) program)
-    )
-      (reverse program))
-    )
-  )
+(print (lil-parse (tokenize "(+ 34 (- 36 1))")))
